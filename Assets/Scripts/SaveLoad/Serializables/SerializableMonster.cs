e5d440a68f2eb9b7b035a13ba9bb00c6b6cbfc3e
EdgeWalker <aegookja@naver.com>
2018-10-27 17:18:45 +0200

    Serialize health and monster name
    Added health serialization
    Added monster name serialization
    Removed duplicate scripts
    Added SetHealth method to both EnemyHealth and PlayerHealth

--- Assets/Scripts/SaveLoad/Serializables/SerializableMonster.cs
+++ Assets/Scripts/SaveLoad/Serializables/SerializableMonster.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using CompleteProject;
+using System;
+using UnityEditor;
 using UnityEngine;
 
 [Serializable]
 public class SerializableMonster : SerializableGameObject
 {
+	public string name;
+	public int health;
+
 	public SerializableMonster(GameObject monster) : base(monster)
 	{
+		name = monster.name;
+
+		EnemyHealth enemyHealth = monster.GetComponent<EnemyHealth>();
+		if (enemyHealth)
+		{
+			health = enemyHealth.currentHealth;
+		}
 	}
 }
--- Assets/Scripts/SaveLoad/Serializables/SerializablePlayer.cs
+++ Assets/Scripts/SaveLoad/Serializables/SerializablePlayer.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using CompleteProject;
+using System;
 using UnityEngine;
 
 [Serializable]
 public class SerializablePlayer : SerializableGameObject
 {
+	public int health;
+
 	public SerializablePlayer(GameObject player) : base(player)
 	{
+		PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+		if (playerHealth)
+		{
+			health = playerHealth.currentHealth;
+		}
 	}
 }
