7d2e673965f4ff252bfa8320604249b77a6f4fb7
EdgeWalker <aegookja@naver.com>
2018-10-27 12:25:38 +0200

    Add save button
    added save button to menu

---
+++ Assets/SaveManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+	private void Start()
+	{
+
+	}
+
+	public void Save()
+	{
+		Debug.Log("Save");
+	}
+}
