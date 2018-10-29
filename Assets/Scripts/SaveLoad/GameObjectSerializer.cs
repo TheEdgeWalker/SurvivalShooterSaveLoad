50a0290bd2601503c88b97801aedca7191d4a0f8
Ji Hoon Kim <aegookja@naver.com>
2018-10-29 17:53:13 +0100

    Add GameObjectSerializer
    Added GameObjectSerializer component to player

---
+++ Assets/Scripts/SaveLoad/GameObjectSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class GameObjectSerializer : MonoBehaviour
+{
+	public bool isInstantiated;
+	public string[] components;
+
+	private void Awake()
+	{
+	}
+}
